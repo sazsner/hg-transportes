46597e167f7a3dc03e8558a0d4576391a70452cb
sazsner <rto.gnz.a@gmail.com>
2025-12-29 00:32:50 -0300

    Se esta ajustando el nav, falta la animacion del sticky bar

--- assets/js/navScroll.js
+++ assets/js/navScroll.js
@@ -1,21 +1,40 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {navScroll()};
 
 // Get the navbar
-let navbar = document.getElementById("navbar");
-let about = document.getElementById("about");
-let linksBox = document.getElementById("linksBox");
+// let navbar = document.getElementById("navbar");
+// let about = document.getElementById("about");
+// let linksBox = document.getElementById("linksBox");
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+
 
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= about.offsetTop) {
-    navbar.classList.add("sticky-top");
-    navbar.classList.add("bg-secondary");
+// function navScroll() {
+//   if (window.pageYOffset >= about.offsetTop) {
+//     navbar.classList.add("navbar-sticky");
+//     navbar.classList.add("bg-secondary");
+//   } else {
+//     navbar.classList.remove("navbar-sticky");
+//     navbar.classList.remove("bg-secondary");
+//   }
+// };
+
+// console.log(navbar, about)
+
+const navbar = document.getElementById("navbar");
+const about = document.getElementById("about");
+
+window.addEventListener("scroll", navScroll);
+
+function navScroll() {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (scrollTop >= about.offsetTop) {
+    navbar.classList.add('show-nav');
+    navbar.classList.add('bg-secondary');
   } else {
-    navbar.classList.remove("sticky-top");
-    navbar.classList.remove("bg-secondary");
+    navbar.classList.remove('show-nav');
+    navbar.classList.remove('bg-secondary');
   }
-}; 
+}
