c9a07b70caca887f1e8d8a255d446fcf6e50cddf
sazsner <rto.gnz.a@gmail.com>
2026-04-06 18:11:13 -0400

    confi preloader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 document.body.classList.add("loading");
 
 window.addEventListener('load', function () {
+  const loader = document.getElementById("loader");
   loader.classList.add("disppear");
-  document.body.classList.remove("loading");
+  // document.body.classList.remove("loading");
+  setTimeout(() => {
+    document.body.classList.remove("loading");
+  }, 100);
 });
